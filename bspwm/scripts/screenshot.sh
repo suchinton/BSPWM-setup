8933f1196c3520f86a212787da51c40872648618
suchinton <75079303+suchinton@users.noreply.github.com>
2022-10-13 19:08:31 +0530

    copy to clipboard after screenshot

--- bspwm/scripts/screenshot.sh
+++ bspwm/scripts/screenshot.sh
@@ -11,9 +11,9 @@ options="$option0\n$option1\n$option2"
 selected="$(echo -e "$options" | rofi -lines 3 -dmenu -p "screen-shot" -config "~/.config/rofi/powermenu.rasi")"
 case $selected in
     $option0)
-        flameshot screen -p ~/Pictures/ ;;
+        flameshot screen -c -p ~/Pictures/ ;;
     $option1)
-        flameshot gui -p ~/Pictures/ ;;
+        flameshot gui -c -p ~/Pictures/ ;;
     $option2)
         cd ~/Pictures/ && sleep 1 && scrot -u;;
 esac
